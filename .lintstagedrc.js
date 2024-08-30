c3994bcfa86d62bff36d486c8a4635e7647747b8
Decebal Dobrica <352761+decebal@users.noreply.github.com>
2024-08-30 22:05:48 +0100

    chore: remove node modules from git

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const buildNextEslintCommand = (filenames) =>
+  `pnpm run --filter=next:lint --fix --file ${filenames
+    .map((f) => path.relative(path.join("packages", "nextjs"), f))
+    .join(" --file ")}`;
+
+const checkTypesNextCommand = () => "pnpm run --filter=next:check-types";
+
+const buildHardhatEslintCommand = (filenames) =>
+  `pnpm run --filter=hardhat:lint-staged --fix ${filenames
+    .map((f) => path.relative(path.join("packages", "hardhat"), f))
+    .join(" ")}`;
+
+module.exports = {
+  "packages/nextjs/**/*.{ts,tsx}": [
+    buildNextEslintCommand,
+    checkTypesNextCommand,
+  ],
+  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
+};
